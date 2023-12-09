d2a569af4f7a342d22ecdf4284070fb2e2d44c5c
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-12-09 22:21:45 +0300

    feat: create the parent view class

---
+++ coffee-house/src/js/views/View.js
@@ -0,0 +1,16 @@
+class View {
+  _data;
+
+  render(data) {
+    this._data = data;
+    const markup = this._generateMarkup();
+    this._clear();
+    this._parentEl.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  _clear() {
+    this._parentEl.innerHTML = "";
+  }
+}
+
+export default View;

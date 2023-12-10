4d14f28ad16d70f43e19944f1df2bc2d4285bec6
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-12-10 21:51:10 +0300

    refactor: use radio buttons for tabs

--- coffee-house/src/js/views/menuTabView.js
+++ coffee-house/src/js/views/menuTabView.js
@@ -2,20 +2,10 @@ import View from "./View";
 
 class MenuTabView extends View {
   _parentEl = document.querySelector(".menu__tabs");
-  _tabs = this._parentEl.querySelectorAll(".btn-tab");
 
   addHandlerChangeTab(handler) {
-    this._parentEl.addEventListener("click", (e) => {
-      const clickedTab = e.target.closest(".btn-tab");
-
-      if (!clickedTab || clickedTab.classList.contains("btn-tab--active"))
-        return;
-
-      this._tabs.forEach((tab) => tab.classList.remove("btn-tab--active"));
-      clickedTab.classList.add("btn-tab--active");
-
-      const { category } = clickedTab.dataset;
-      handler(category, this._isBigScreen());
+    this._parentEl.addEventListener("change", (e) => {
+      handler(e.target.value, this._isBigScreen());
     });
   }
 }
